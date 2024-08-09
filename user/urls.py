25ca4a0d6dfd84edba5e3d18b9576ef2df4e2c20
Yash Dodiya <yashdodiya1707@gmail.com>
2024-08-09 20:25:10 +0530

    updated user/urls

--- user/urls.py
+++ user/urls.py
@@ -3,13 +3,22 @@ from rest_framework.routers import DefaultRouter
 
 from .views import RegisterAPIView, LoginAPIView, ChatbotViewSet, UploadPDFView, ChatAPIView
 
+# APIs for chatbot creation with the chatbot name
 router = DefaultRouter()
 router.register(r'chatbots', ChatbotViewSet)
 
 urlpatterns = [
+    # API for user Registration
     path('register/', RegisterAPIView.as_view(), name='register'),
+
+    # API for user Login
     path('login/', LoginAPIView.as_view(), name='login'),
+
+    # API for pdf upload with chatbot name
     path('upload-pdf/', UploadPDFView.as_view(), name='upload-pdf'),
+
+    # API for chatbot have to give a name
     path('chat-api/', ChatAPIView.as_view(), name='chat-api'),
+
     path('', include(router.urls)),
 ]
